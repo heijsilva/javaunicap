a709e8d824ff571c1c52c0327a86f31220801865
heijsilva <132012711+heijsilva@users.noreply.github.com>
2025-02-18 23:52:02 +0000

    Correçao no codigo

---
+++ Java/exemplo.java
@@ -0,0 +1,14 @@
+public class exemplo{
+    public static void main(String[] args) {
+        // System.out.printIn("Ola mundo !!");
+        System.out.print("O Dobro de 10.5 é " + dobro(10.5));
+    }
+    
+    public static int dobro(int num) {
+        return 2 * num;
+    }
+
+    public static double dobro(double num) {
+        return 2 * num;
+    }
+}
