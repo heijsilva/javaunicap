33ca70fe7eb3d66790249b4433ff6b36b5e9c682
heijsilva <132012711+heijsilva@users.noreply.github.com>
2025-02-19 00:26:22 +0000

    Criaçao de inputs

--- Java/Exemplo.java
+++ Java/Exemplo.java
@@ -1,4 +1,4 @@
-public class exemplo{
+public class Exemplo{
     public static void main(String[] args) {
         // System.out.printIn("Ola mundo !!");
         System.out.print("O Dobro de 10.5 é " + dobro(10.5));
---
+++ exemplo2/Exemplo2.java
@@ -0,0 +1,8 @@
+public class Exemplo2{
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 10;
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
