2f3507d7b640a6a020b1c63a13a51500d47058c6
heijsilva <132012711+heijsilva@users.noreply.github.com>
2025-02-19 00:26:34 +0000

    Input

--- exemplo2/Exemplo2.java
+++ exemplo2/Exemplo2.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Exemplo2{
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 10;
-        System.out.println(num1);
-        System.out.println(num2);
+
+        System.out.print("Digite seu numero");
+        num2 = input.nextInt();
+
+        System.out.println("Numero 1 = " + num1);
+        System.out.println("Numero 2 = " + num2);
+    
     }
 }
