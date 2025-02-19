b185d4b195ce13d9fa92c13437ca703246617a5b
heijsilva <132012711+heijsilva@users.noreply.github.com>
2025-02-19 00:31:57 +0000

    Variavel Double

--- exemplo2/Exemplo2.java
+++ exemplo2/Exemplo2.java
@@ -5,12 +5,17 @@ public class Exemplo2{
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 10;
+        double num3 = 3.15, num4;
 
         System.out.print("Digite seu numero");
         num2 = input.nextInt();
 
+        System.out.print("Digite seu numero");
+        num4 = input.nextDouble();
+
         System.out.println("Numero 1 = " + num1);
         System.out.println("Numero 2 = " + num2);
-    
+        System.out.println("Numero 3 = " + num3);
+        System.out.println("Numero 4 = " + num3);
     }
 }
